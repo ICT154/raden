9b039bd6f7c8e4d7c8224d9e9967fa7484f5b69e
R4D3N G0Z4LL <radengozal@gmail.com>
2024-08-08 19:31:02 +0700

    Update error pages to remove unnecessary code and improve maintenance page layout

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -8,8 +8,7 @@
             <div class="col-md-8 offset-md-2">
                 <h3>Internal Server Error</h3>
                 <p class="sub-content">The page you are attempting to reach is currently not available. This may be
-                    because the page does not exist or has been moved.</p><a class="btn btn-primary"
-                    href="{{ route('dashboard.index') }}">BACK TO HOME PAGE</a>
+                    because the page does not exist or has been moved.</p>
             </div>
         </div>
     </div>
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+@include('templates.components.vendor-css')
+
+<!-- page-wrapper Start-->
+<div class="page-wrapper">
+    <!-- Maintenance start-->
+    <div class="error-wrapper maintenance-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="maintenance-heading">
+                        <h2 class="headline">MAINTENANCE</h2>
+                    </div>
+                    <h4 class="sub-content">Our Site is Currently under maintenance We will be back Shortly<br> Thank You
+                        For Patience</h4>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Maintenance end-->
+</div>
